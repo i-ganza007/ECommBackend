2d535a215505a9772e42770342785b058c5aedfd
i-ganza007 <i.ganza@alustudent.com>
2026-09-02 09:55:28 +0200

    fix: adding repo services

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using ECommBackend.CustomErrors.ExceptionFilterLayer;
 using ECommBackend.DatabaseConns;
 using ECommBackend.Models;
+using ECommBackend.Repositories;
+using ECommBackend.Repositories.RepoInterfaces;
+using ECommBackend.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +14,17 @@ builder.Services.AddDbContext<SQLiteConn>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("ECommSQLite"));
 });
+builder.Services.AddScoped<IProductRepo,ProductRepo>();
+builder.Services.AddScoped<IUserRepo, UserRepo>();
+builder.Services.AddScoped<IAdminRepo, AdminRepo>();
+builder.Services.AddScoped<IOrderRepo, OrderRepo>();
+builder.Services.AddScoped<IImageRepo, ImageRepo>();
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<AdminService>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<OrderService>();
+builder.Services.AddScoped<ImageService>();
+
 //builder.Services.AddDbContext<SQLConn>(options =>
 //{
 //    options.UseSql(builder.Configuration.GetConnectionString("ECommSQLite"));
